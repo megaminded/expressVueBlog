e210458ce3f7cf0c55dfe4b1316cd15dd81dbba3
unknown <megamindsetsolution@gmail.com>
2019-04-26 01:23:18 +0100

    Installed bootstrap and Further completed front end looks by inluding bootstrap css, created a navbar, and better grouped components in folders

---
+++ frontend/src/components/partials/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<a class="navbar-brand" href="/">Navbar</a>
+			<button
+				class="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNav"
+				aria-controls="navbarNav"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<router-link class="nav-link" to="/blog">Blog</router-link>
+					</li>
+					<li class="nav-item">
+						<router-link class="nav-link" to="/about">About</router-link>
+					</li>
+				</ul>
+				<ul class="navbar-nav navbar-right">
+					<li class="nav-item">
+						<router-link class="nav-link" to="/login">Login</router-link>
+					</li>
+					<li class="nav-item">
+						<router-link class="nav-link" to="/register">Register</router-link>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+</template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,13 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import css from 'bootstrap/dist/css/bootstrap.min.css'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  css,
+  components: {
+    App
+  },
   template: '<App/>'
 })
